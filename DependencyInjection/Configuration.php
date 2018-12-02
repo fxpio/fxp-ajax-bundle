b0032f53d97de7216356c9c9254c30642f99edcc
François Pluchino <francois.pluchino@gmail.com>
2018-12-02 15:10:58 +0100

    Update to Symfony 4.2

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -11,6 +11,7 @@
 
 namespace Fxp\Bundle\AjaxBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -26,13 +27,15 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('fxp_ajax');
+        $treeBuilder = new TreeBuilder('fxp_ajax');
+        /* @var ArrayNodeDefinition $rootNode */
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
-        ->children()
-            ->scalarNode('ajax_id_parameter')->defaultValue('ajax_id')->cannotBeEmpty()->end()
-        ->end();
+            ->children()
+                ->scalarNode('ajax_id_parameter')->defaultValue('ajax_id')->cannotBeEmpty()->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
